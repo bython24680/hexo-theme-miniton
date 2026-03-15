6b48aee4f673113d86800abb4786ff56547cc553
Bython <bython24680@gmail.com>
2026-03-15 20:03:18 +0800

    feat: Support more social media links and customized links #9

--- scripts/index.js
+++ scripts/index.js
@@ -6,20 +6,43 @@ hexo.extend.helper.register("social_links", function (themeSocials, styleType) {
   ];
   let styleType0LinkSettings = {
     github: 'hover:text-[#333] dark:hover:text-white',
+    threads: 'hover:text-black dark:hover:text-white',
+    facebook: 'hover:text-[#1877F2]',
     twitter: 'hover:text-[#1DA1F2]',
+    bluesky: 'hover:text-[#0281F7]',
+    linkedin: 'hover:text-[#0A66C2]',
+    line: 'hover:text-[#06C755]',
+    duolingo: 'hover:text-[#58CC02]',
+    instagram: 'hover:text-[#E4405F]',
     email: 'hover:text-primary',
+    custom_links: 'hover:text-[#333] dark:hover:text-white',
   };
   let icons = {
     github: 'fab fa-github',
     twitter: 'fab fa-x-twitter',
+    threads: 'fab fa-threads',
+    facebook: 'fab fa-facebook',
+    bluesky: 'fab fa-bluesky',
+    linkedin: 'fab fa-square-linkedin',
+    line: 'fab fa-line',
+    duolingo: 'fab fa-duolingo',
+    instagram: 'fab fa-instagram',
     email: 'fas fa-envelope',
   };
+  let defaultCustomLinkIcon = 'fas fa-link';
 
   let styles = styleTypes[styleType] ?? styleTypes[2];
   let specialSettings = styleType === 0 ? styleType0LinkSettings : {};
   let socials = [
     'github',
+    'linkedin',
+    'duolingo',
     'twitter',
+    'instagram',
+    'threads',
+    'facebook',
+    'bluesky',
+    'line',
     'email',
   ];
 
@@ -36,6 +59,23 @@ hexo.extend.helper.register("social_links", function (themeSocials, styleType) {
       htmlString += '</a>'
     }
   });
+
+  // custom links
+  if (themeSocials.custom_links) {
+    let linkStyle = styles;
+    if (styleType == 0) {
+      linkStyle += ' ' + specialSettings.custom_links;
+    }
+    themeSocials.custom_links.forEach(function (linkConfig) {
+      if (linkConfig.url) {
+        let icon = linkConfig.icon ?? defaultCustomLinkIcon;
+
+        htmlString += '<a href="' + linkConfig.url + '" class="' + linkStyle + '">';
+        htmlString += '<i class="' + icon + '"></i>';
+        htmlString += '</a>';
+      }
+    });
+  }
 
   return htmlString;
 });
