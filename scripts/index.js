45381ebd09b159468a4881f37b207ad6cfe95be0
Bython <bython24680@gmail.com>
2026-03-15 17:19:05 +0800

    refactor: Reduce style settings #9

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,22 @@
 hexo.extend.helper.register("social_links", function (themeSocials, styleType) {
   let styleTypes = [
-    {
-      github: 'text-slate-400 hover:text-[#333] dark:hover:text-white transform hover:scale-110 transition-all text-2xl',
-      twitter: 'text-slate-400 hover:text-[#1DA1F2] transform hover:scale-110 transition-all text-2xl',
-      email: 'text-slate-400 hover:text-primary transform hover:scale-110 transition-all text-2xl',
-    },
-    {
-      github: 'bg-slate-100 dark:bg-slate-700 text-slate-600 dark:text-slate-300 p-2 rounded hover:bg-primary hover:text-white transition-colors',
-      twitter: 'bg-slate-100 dark:bg-slate-700 text-slate-600 dark:text-slate-300 p-2 rounded hover:bg-primary hover:text-white transition-colors',
-      email: 'bg-slate-100 dark:bg-slate-700 text-slate-600 dark:text-slate-300 p-2 rounded hover:bg-primary hover:text-white transition-colors',
-    },
-    {
-      github: 'text-slate-400 hover:text-primary transition-colors',
-      twitter: 'text-slate-400 hover:text-primary transition-colors',
-      email: 'text-slate-400 hover:text-primary transition-colors',
-    }
+    'text-slate-400 transform hover:scale-110 transition-all text-2xl',
+    'bg-slate-100 dark:bg-slate-700 text-slate-600 dark:text-slate-300 p-2 rounded hover:bg-primary hover:text-white transition-colors',
+    'text-slate-400 hover:text-primary transition-colors',
   ];
+  let styleType0LinkSettings = {
+    github: 'hover:text-[#333] dark:hover:text-white',
+    twitter: 'hover:text-[#1DA1F2]',
+    email: 'hover:text-primary',
+  };
   let icons = {
     github: 'fab fa-github',
     twitter: 'fab fa-x-twitter',
     email: 'fas fa-envelope',
   };
 
-  let styles = styleTypes[styleType] ?? styleTypes[0];
+  let styles = styleTypes[styleType] ?? styleTypes[2];
+  let specialSettings = styleType === 0 ? styleType0LinkSettings : {};
   let socials = [
     'github',
     'twitter',
@@ -31,8 +25,13 @@ hexo.extend.helper.register("social_links", function (themeSocials, styleType) {
 
   let htmlString = '';
   socials.forEach(function (socialName) {
-    if (themeSocials[socialName] && styles[socialName] && icons[socialName]) {
-      htmlString += '<a href="' + themeSocials[socialName] + '" class="' + styles[socialName] + '">';
+    if (themeSocials[socialName] && styles && icons[socialName]) {
+      let linkStyle = styles;
+      if (specialSettings[socialName] ?? '') {
+        linkStyle += ' ' + specialSettings[socialName];
+      }
+
+      htmlString += '<a href="' + themeSocials[socialName] + '" class="' + linkStyle + '">';
       htmlString += '<i class="' + icons[socialName] + '"></i>';
       htmlString += '</a>'
     }
